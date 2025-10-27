fffe4c64a02946226f0ed63b217e58dcc4c2e421
Matheus Rodrigues  Vieira <matheus@waytech.com.br>
2025-10-27 16:08:35 -0300

    Config react routes

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Home() {
+    return <div>
+        <h1>H</h1>
+        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">Entrar</button>
+    </div>
+}
+
+export default Home;
